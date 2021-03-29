33d38302381c90efad8a638f763b34d63eee5599
Chantz-J <johnson.chantz@gmail.com>
2021-03-29 15:25:17 -0400

    Changed Homepage styling. Will work on this in separate branch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import styled from 'styled-components'
+import { Button, Container } from 'semantic-ui-react'
 import { testClick } from '../actions/index'
 import { connect } from 'react-redux'
 
+
+
 import Banner from './Banner'
+
+const StyledMain = styled.main`
+    display: flex;
+`
 
 const Home = (props) => {
     const { testClick, isLoading } = props
     return (
-        <>
-            <Banner />
-            <h1>Hello World!</h1>
-            <Button loading={isLoading} color='blue' onClick={() => testClick()} >Test Button</Button>
-        </>
+        <StyledMain>
+            <Container>
+                <h1>Hello World!</h1>
+                <Button loading={isLoading} color='blue' onClick={() => testClick()} >Test Button</Button>
+            </Container>
+            <Container>
+                <Banner/>
+            </Container>
+        </StyledMain>
     )
 }
 
